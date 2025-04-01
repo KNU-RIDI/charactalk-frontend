b567ddd1133cfd2c602d1dd198ac5497b3eeff1e
sue <ysh@knu.ac.kr>
2025-04-01 14:37:41 +0900

    feat: 프로필 컴포넌트 링크 연결

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,15 +1,17 @@
-import Link from "next/link"
+import { Link } from "react-router-dom"
 import { Avatar, AvatarImage } from "@/components/ui/avatar"
 
 const Profile = () => {
   return (
-    <Avatar className="h-9 w-9 cursor-pointer">
-      <AvatarImage
-        src="https://github.com/user-attachments/assets/1f81de33-1b45-45b4-8474-ad33dc558e08"
-        alt="Profile avatar"
-        className="object-cover"
-      />
-    </Avatar>
+    <Link to="/my-page">
+      <Avatar className="h-9 w-9 cursor-pointer">
+        <AvatarImage
+          src="https://github.com/user-attachments/assets/1f81de33-1b45-45b4-8474-ad33dc558e08"
+          alt="Profile avatar"
+          className="object-cover"
+        />
+      </Avatar>
+    </Link>
   )
 }
 
