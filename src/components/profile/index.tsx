255bb8d928e24b2eaa7e7371074cdeb482e460f5
go-minjung <a01086712346@gmail.com>
2025-04-02 18:25:47 +0900

    feat: 로그인 경로 수정

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,9 +1,25 @@
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { Avatar, AvatarImage } from "@/components/ui/avatar"
+import { useEffect, useState } from "react"
 
 const Profile = () => {
+  const navigate = useNavigate()
+  const [token, setToken] = useState<string | null>(null)
+
+  useEffect(() => {
+    setToken(localStorage.getItem("accessToken"))
+  }, [])
+  const handleClick = () => {
+    if (token) {
+      navigate("/my-page")
+    } else {
+      window.location.href =
+        "https://api.charactalk.site/auth/login/google?redirect=http://localhost:3000"
+    }
+  }
+
   return (
-    <Link to="/my-page">
+    <button onClick={handleClick}>
       <Avatar className="h-9 w-9 cursor-pointer">
         <AvatarImage
           src="https://github.com/user-attachments/assets/1f81de33-1b45-45b4-8474-ad33dc558e08"
@@ -11,7 +27,7 @@ const Profile = () => {
           className="object-cover"
         />
       </Avatar>
-    </Link>
+    </button>
   )
 }
 
