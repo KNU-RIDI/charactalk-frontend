a12cdab82008e7255cc530db423dd927365e3159
go-minjung <a01086712346@gmail.com>
2025-04-05 19:30:33 +0900

    feat: layout SidebarTrigger 수정

--- src/components/Sidebar/layout.tsx
+++ src/components/Sidebar/layout.tsx
@@ -3,12 +3,15 @@ import { AppSidebar } from "@/components/Sidebar/app-sidebar"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
-    </SidebarProvider>
+    <div className="flex h-screen w-screen">
+      {/* 사이드바 */}
+      <SidebarProvider>
+        <AppSidebar />
+        <div className="relative flex flex-1 flex-col overflow-hidden">
+          <SidebarTrigger />
+          {children}
+        </div>
+      </SidebarProvider>
+    </div>
   )
 }
