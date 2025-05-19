8c771e8744c5d472ca41667b934cdc48aa1949c9
윤수현 <ysh@knu.ac.kr>
2025-05-19 22:01:20 +0900

    feat: 채팅방 생성 관련 types 정의

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,23 @@ export interface Message {
   isTyping: boolean
   timestamp: string
 }
+
+export type ChatRoomType = "SINGLE" | "GROUP"
+
+export interface CreateChatRoomRequest {
+  characterId: number
+  name: string
+  type: ChatRoomType
+}
+
+export interface CreateChatRoomResponse {
+  chatRoomId: number
+  character: CharacterSummary
+}
+
+export interface CharacterSummary {
+  characterId: number
+  name: string
+  description: string
+  imageUrl: string
+}
