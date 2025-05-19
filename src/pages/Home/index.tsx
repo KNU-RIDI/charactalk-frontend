e4dce7dc196a7f523a8a6a1b88f02229d279a978
go-minjung <a01086712346@gmail.com>
2025-05-20 02:25:08 +0900

    feat: Layout 삭제, Sidebar 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,25 @@
-import Layout from "@/components/Sidebar/layout"
 import Profile from "@/components/Profile"
 import BestCharacters from "./components/bestChar"
 import BookSection from "./components/Book"
 import Search from "@/components/Search"
+import Sidebar from "@/components/Sidebar/sidebar"
 
 export default function Home() {
   return (
-    <div className="relative flex h-screen overflow-hidden bg-white">
-      <div className="absolute top-4 right-4 z-10">
-        <Profile></Profile>
-      </div>
-      <div className="absolute top-4 right-15 z-5">
-        <Search></Search>
-      </div>
-      <Layout>
-        <div className="w-full px-6">
-          <h1 className="mb-6 text-xl font-bold" style={{ color: "#1E9EFF" }}>
-            CharacTalk
-          </h1>
-
-          <BestCharacters></BestCharacters>
-          <BookSection></BookSection>
+    <div className="flex h-screen bg-white">
+      <Sidebar />
+      <div className="flex flex-1 flex-col overflow-y-auto px-6 py-4">
+        <div className="mb-6 flex items-center justify-between">
+          <h1 className="text-xl font-extrabold text-[#1E9EFF]">CharacTalk</h1>
+          <div className="flex items-center gap-4">
+            <Search />
+            <Profile />
+          </div>
         </div>
-      </Layout>
+
+        <BestCharacters />
+        <BookSection />
+      </div>
     </div>
   )
 }
